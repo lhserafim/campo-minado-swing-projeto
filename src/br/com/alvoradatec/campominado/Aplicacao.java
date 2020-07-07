0ae052f64689019c1694db88a2fcd8d54da486c0
Luiz Henrique Sanches Serafim <a29061@somoseducacao.com.br>
2020-07-07 06:10:03 -0300

    projeto criado

---
+++ src/br/com/alvoradatec/campominado/Aplicacao.java
@@ -0,0 +1,12 @@
+package br.com.alvoradatec.campominado;
+
+import br.com.alvoradatec.campominado.modelo.Tabuleiro;
+import br.com.alvoradatec.campominado.visao.TabuleiroConsole;
+
+public class Aplicacao {
+    public static void main(String[] args) {
+
+        Tabuleiro tabuleiro = new Tabuleiro(6, 6, 3);
+        new TabuleiroConsole(tabuleiro);
+    }
+}
